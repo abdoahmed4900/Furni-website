bcf5f54189d3917eba41d7f3508843efee23e813
Mashaly0 <abdullahmashaly90@gmail.com>
2025-04-05 00:46:36 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSiderbar(){
+    let sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+    }
+    function hideSiderbar(){
+    let sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+    }
+
